c37e5c98a23585b6926ef8e9b9ab01a0dd5c35ba
Ben <westb2@rpi.edu>
2016-04-20 18:18:29 -0400

    added exception handling line by line

--- process_spreadsheet.py
+++ process_spreadsheet.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-assumed_header = ['Date/Time,Tournamemnt,Opponent,Point Elapsed Seconds,Line,Our Score - End of Point,Their Score - End of Point,Event Type,Action,Passer,Receiver,Defender,Hang Time (secs),Player 0,Player 1,Player 2,Player 3,Player 4,Player 5,Player 6,Player 7,Player 8,Player 9,Player 10,Player 11,Player 12,Player 13,Player 14,Player 15,Player 16,Player 17,Player 18,Player 19,Player 20,Player 21,Player 22,Player 23,Player 24,Player 25,Player 26,Player 27,Elapsed Time (secs)']
+assumed_header = 'Date/Time,Tournamemnt,Opponent,Point Elapsed Seconds,Line,Our Score - End of Point,Their Score - End of Point,Event Type,Action,Passer,Receiver,Defender,Hang Time (secs),Player 0,Player 1,Player 2,Player 3,Player 4,Player 5,Player 6,Player 7,Player 8,Player 9,Player 10,Player 11,Player 12,Player 13,Player 14,Player 15,Player 16,Player 17,Player 18,Player 19,Player 20,Player 21,Player 22,Player 23,Player 24,Player 25,Player 26,Player 27,Elapsed Time (secs)'
 
 #this function takes a filepath to a spreadsheet from ultistats. It
 #then computes statistics and returns them in players.
@@ -10,46 +10,49 @@ def process_spreadsheet(fname):
 	#we open the csv and prepare to read it line by line
 
 	with open(fname, 'rb') as csvfile:
-		linereader = csv.reader(csvfile, delimiter="\n")
-		header = next(linereader)
 		try:
+			header = csvfile.readline().rstrip()
 			if header != assumed_header: 
-				raise Exception("Exception: the header for " + fname +"is not what we thought")
-			#We have opened the file and read out the header. Now we read line
-			#by line
+				raise Exception("Exception: the header for " + fname +" is not what we thought")
+					
 			previousLine = None
 			lineSuccess = []
-			for line_ in linereader:
-				statRecorded = set()
-				line = line_[0].split(",")
-				#compute statistics here.
-				#It is important that trackgames comes first as it is responsable
-				#for instantiating players into the structure
-				trackGames(players, line)
-				if previousLine != None:
-					#put all statistics that need previous line here
-					statRecorded.add(trackTouches(players, line, previousLine))
-					statRecorded.add(trackStalls(players, line))
-					statRecorded.add(trackThrowaways(players, line))
-					statRecorded.add(trackDs(players, line))
-					statRecorded.add(trackDrops(players, line))
-					statRecorded.add(trackPenalties(players, line))
+			for lineNum, line_ in enumerate(csvfile):
+				#We have opened the file and read out the header. Now we read line
+				#by line					
+				try:
+					statRecorded = set()
+					line = line_.rstrip().split(",")
+					#compute statistics here.
+					#It is important that trackgames comes first as it is responsable
+					#for instantiating players into the structure
+					trackGames(players, line)
+					if previousLine != None:
+						#put all statistics that need previous line here
+						statRecorded.add(trackTouches(players, line, previousLine))
+						statRecorded.add(trackStalls(players, line))
+						statRecorded.add(trackThrowaways(players, line))
+						statRecorded.add(trackDs(players, line))
+						statRecorded.add(trackDrops(players, line))
+						statRecorded.add(trackPenalties(players, line))
 
-					a = None
-				
-				if (len(line)>15) and (line[12]!="" or previousLine is None or line[5]!=previousLine[5] or line[6]!=previousLine[6]):
-						#put all statistics that only get counted once per point here
-						trackSeconds(players, line)
-						trackOPoints(players, line)
-						trackDPoints(players, line)
-						trackOConversions(players, line, previousLine)
-						trackDConversions(players, line, previousLine)
-						statRecorded.add(trackPulls(players, line))
-						statRecorded.add(trackPullHangtime(players, line))
-				#this is the last thing we do each loop
-				lineSuccess.append(True in statRecorded)
-				
-				previousLine = line
+						a = None
+					
+					if (len(line)>15) and (line[12]!="" or previousLine is None or line[5]!=previousLine[5] or line[6]!=previousLine[6]):
+							#put all statistics that only get counted once per point here
+							trackSeconds(players, line)
+							trackOPoints(players, line)
+							trackDPoints(players, line)
+							trackOConversions(players, line, previousLine)
+							trackDConversions(players, line, previousLine)
+							statRecorded.add(trackPulls(players, line))
+							statRecorded.add(trackPullHangtime(players, line))
+					#this is the last thing we do each loop
+					lineSuccess.append(True in statRecorded)
+					
+					previousLine = line
+				except Exception as inst:
+					print "Exception raised on line "+str(lineNum+2)
 		except Exception as inst:
 			print inst.args
 	return [players, lineSuccess]
@@ -249,7 +252,7 @@ def trackPulls(players, line):
 		players[line[11]]["pulls"]+=1
 		return True
 	else:
-		print "No one to give this pull to"
+		#print "No one to give this pull to"
 		return False
 
 def trackPullHangtime(players, line):
