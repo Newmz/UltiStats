dbb0a071b24199d111e317bb28a8018a82a258cd
Ben <westb2@rpi.edu>
2016-05-04 17:20:52 -0400

    Allowed for running app.py with a port number argument

--- Workspace/app.py
+++ Workspace/app.py
@@ -1,13 +1,16 @@
 import SimpleHTTPServer
 import SocketServer
 import webbrowser
+import sys
+PORT = 8080
 
-PORT = 8000
+if len(sys.argv)>1:
+	PORT = int(sys.argv[1])
 
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
-webbrowser.open("http://localhost:8000/demo.html")
+webbrowser.open("http://localhost:" + str(PORT) + "/demo.html")
 httpd.serve_forever()
